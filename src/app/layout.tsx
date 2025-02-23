75a666f6365213cd389b01011fa91146edf94a7d
Pablerdo <pablosalamanca88@gmail.com>
2025-02-23 17:31:15 -0500

    change sidebar name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Septaframe",
+  title: "Zeptaframe",
   description: "AI-native controllable video editor",
 };
 
