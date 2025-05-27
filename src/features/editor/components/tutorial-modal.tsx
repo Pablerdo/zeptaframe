92156da052d86fff7c8e3ffaa12d1827020701d6
Pablerdo <pablosalamanca88@gmail.com>
2025-05-27 12:01:52 -0400

    add tutorial video

--- src/features/editor/components/tutorial-modal.tsx
+++ src/features/editor/components/tutorial-modal.tsx
@@ -1,13 +1,24 @@
-
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
 export const TutorialModal = ({ isOpen, onClose }: { isOpen: boolean, onClose: () => void }) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>
+      <DialogContent className="max-w-4xl w-full">
         <DialogHeader>
           <DialogTitle>Tutorial</DialogTitle>
         </DialogHeader>
+        <div className="mt-4">
+          <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+            <iframe
+              className="absolute top-0 left-0 w-full h-full rounded-lg"
+              src="https://www.youtube.com/embed/yAPVlnxwhaw"
+              title="Tutorial Video"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
